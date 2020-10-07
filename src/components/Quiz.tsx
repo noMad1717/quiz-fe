35db6ce8362a216783b08edc928cf8f100d1c461
Erik Jonsson <erik.jonsson17@gmail.com>
2020-10-07 20:48:07 +0200

    Add answer-checking

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -10,6 +10,8 @@ const questions: Question[] = require('../resources/data.json')
 const Quiz: React.FC = () => {
     const [question, setQuestion] = useState<Question> (questions[0])
     const [inputVal, setInputVal] = useState<string>('')
+    const [lives, setLives] = useState<number>(3)
+
     const inputRef = React.useRef<HTMLInputElement | null>(null)
     const buttonRef = React.useRef<HTMLButtonElement | null>(null)
 
@@ -34,10 +36,18 @@ const Quiz: React.FC = () => {
         }
     }, [])
 
+    const checkAnswer = (): void => {
+        const match: string | undefined = question.answers.find(a => a.trim().toLowerCase() === inputVal.trim().toLowerCase())
+        if (!match) {
+            setLives(lives - 1)
+        }
+    }
+
     const getNextQuestion = (): void => {
         const currIndex: number = questions.indexOf(question)
         const nextIndex: number = currIndex < questions.length - 1 ? currIndex + 1: 0
 
+        checkAnswer()
         setInputVal('')
         setQuestion(questions[nextIndex])
     }
@@ -47,6 +57,7 @@ const Quiz: React.FC = () => {
             <h2>{question.question}</h2>
             <input ref={inputRef} type='text' value={inputVal} onChange={(e: React.FormEvent<HTMLInputElement>) => setInputVal(e.currentTarget.value)}/>
             <button ref={buttonRef} onClick={(e: React.FormEvent<HTMLButtonElement>) => getNextQuestion()}>Submit</button>
+            <p>Lives: {lives}</p>
         </div>
     )
 }
